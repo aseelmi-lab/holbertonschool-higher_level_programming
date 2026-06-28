7a6b3d5288c3b725f8054dfdb9adb7ba9249f3b4
aseelmi-lab <aseel.mashall@gmail.com>
2026-06-28 06:31:14 +0000

    Add task 12: pascal_triangle function

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that defines the pascal_triangle function."""
+
+
+def pascal_triangle(n):
+    """Returns Pascal's triangle of n as a list of lists.
+
+    Args:
+        n (int): The number of rows of Pascal's triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
