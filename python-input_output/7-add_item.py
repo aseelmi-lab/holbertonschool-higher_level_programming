fd78a1370adbf1e460e0b7213c6b1ae6ed329935
aseelmi-lab <aseel.mashall@gmail.com>
2026-06-28 06:32:49 +0000

    Fix task 7: move import os to top

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Script that adds arguments to a list and saves them to a JSON file."""
+import os
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import os
 
 filename = "add_item.json"
 
