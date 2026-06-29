16334c663e30533f6e4dabd1571d996bde7ff853
aseelmi-lab <aseel.mashall@gmail.com>
2026-06-29 08:46:04 +0200

    Add task 0: basic JSON serialization

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for basic JSON serialization and deserialization."""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serializes a Python dictionary to a JSON file.
+
+    Args:
+        data (dict): The Python dictionary to serialize.
+        filename (str): The output JSON filename.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """Loads and deserializes data from a JSON file.
+
+    Args:
+        filename (str): The input JSON filename.
+
+    Returns:
+        dict: The deserialized Python dictionary.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
